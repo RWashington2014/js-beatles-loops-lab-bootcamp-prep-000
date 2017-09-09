2b9fbb5fc964e5602ff491af82498f9561960eeb
Robert W <robwash@gmail.com>
2017-09-09 20:09:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,46 @@
+//More Looping
+
+function theBeatlesPlay(musicans, instruments){
+
+    var testArray = []
+
+
+    for (var i = 0; i < musicans.length; i++) {
+
+      var string = `${musicans[i]} plays ${instruments[i]}`
+      testArray = [...testArray, string]
+    }
+    return testArray
+}
+
+function johnLennonFacts(facts){
+
+    var testArray = []
+    var string = "!!!"
+    var i = 0
+
+    while (facts.length > 0 && i < facts.length) {
+
+      facts[i] = `${facts[i]}${string}`
+      facts = [...facts, facts[i]]
+
+      testArray = [...testArray, facts[i]]
+      facts.slice(1)
+      i++
+    }
+      return testArray
+}
+
+function iLoveTheBeatles(number){
+
+    var testArray = []
+    do {
+      var string = "I love the Beatles!"
+      testArray.push(string)
+      ++number
+
+    } while(number < 15);
+
+    return testArray
+
+}
